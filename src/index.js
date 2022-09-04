86fdbb665fec0a045f5fbe0c4b7bd9301453af2d
Jean Caiza <jeanmcm@hotmail.com>
2022-09-04 12:45:32 -0500

    test graphql

--- src/index.js
+++ src/index.js
@@ -1,10 +1,62 @@
-const express = require("express");
-const app = express();
+const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServerPluginLandingPageLocalDefault } = require("apollo-server-core");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: "bounded",
+  /**
+   * What's up with this embed: true option?
+   * These are our recommended settings for using AS;
+   * they aren't the defaults in AS3 for backwards-compatibility reasons but
+   * will be the defaults in AS4. For production environments, use
+   * ApolloServerPluginLandingPageProductionDefault instead.
+   **/
+  plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
 });
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
 });
